e5383592b9d43942a09ab00ab149bb216bd78876
AudunTA <aud-and2@online.no>
2022-12-15 02:44:31 +0100

    hjemme

--- src/DOM/insertListing.mjs
+++ src/DOM/insertListing.mjs
@@ -1,8 +1,12 @@
 export function DOM_listing (DOM_element, DOM_object) {
     console.log(DOM_object);
+    const endDate = convertDate(DOM_object.endsAt);
     DOM_element.innerHTML +=`<div class="col-sm-6 col-lg-4 mb-3">
     <div class="card w-100 h-100">
-      <img src="${DOM_object.media}" onerror="this.src = 'https://www.macmillandictionary.com/us/external/slideshow/full/Grey_full.png';" class="card-img-top" alt="Auction Item 2">
+    <div class="end position-absolute bg-info rounded">
+    <p  class="m-1  p-1">ending: ${endDate}</p>
+  </div>
+      <img src="${DOM_object.media[0]}" onerror="this.src = 'https://www.macmillandictionary.com/us/external/slideshow/full/Grey_full.png';" class="card-img-top" alt="Auction Item 2">
       <div class="card-body d-flex flex-column justify-content-between">
       <div>
         <h5 class="card-title">${DOM_object.title}</h5>
@@ -15,3 +19,16 @@ export function DOM_listing (DOM_element, DOM_object) {
   console.log(DOM_object.id);
     console.log(DOM_object.title);
 };
+
+function convertDate(isoString) {
+  const date = new Date(isoString.slice(0, -1));
+  const todaysDate = new Date();
+  if(todaysDate > date) {
+    return "Expired";
+  } else {
+      const stringDate = date.toString();
+  return stringDate.slice(0,25);
+
+  }
+
+}
--- src/index.mjs
+++ src/index.mjs
@@ -8,16 +8,31 @@ import { signOut } from "./auth/signout.mjs";
 import { getAvatarLoggedIn } from "./auth/status.mjs";
 import { postListing } from "./listings/createListing.mjs";
 console.log(baseUrl);
+// login /sign up inputs
 const userLoggedIn = document.querySelector(".user-logIn");
 const btn_signUp = document.querySelector("#btnHeader_signUp");
 const btn_addBalance = document.querySelector("#btn_addBalance");
-const DOM_credits = document.querySelector("#user_credit");
+const logOut = document.querySelector(".log-out");
 
+//credit inputs are used to fill inn the DOM field on certain pages.
+const DOM_credits = document.querySelector("#user_credit");
+// search inputs/buttons, one for the header and one for mobile
 const btn_searchHeader = document.querySelector("#btn_searchH");
 const input_searchHeader = document.querySelector("#search_fieldH");
 const btn_searchMobile = document.querySelector("#btn_searchM");
 const input_searchMobile = document.querySelector("#search_fieldM");
+
+//input for create form
+const create_form = document.querySelector("#create_form");
+const input_title = document.querySelector("#create_title");
+const input_description = document.querySelector("#create_description");
+const input_tags = document.querySelector("#create_tags");
+const input_media = document.querySelector("#create_media");
+const input_ends = document.querySelector("#create_endsAt");
+
+//checks if user is logged in
 if(getToken()) {
+    //runs if user is logged in
     const username = localStorage.getItem("username");
     const credits = localStorage.getItem("credit");
     userLoggedIn.style.display="block";
@@ -28,6 +43,7 @@ if(getToken()) {
     updateUserDOM(username, credits);
 }
     else {
+        //runs if user is not logged in
         console.log("not logged in");
         btn_addBalance.disabled = true;
         DOM_credits.innerHTML="log in to view "
@@ -37,7 +53,6 @@ if(getToken()) {
 
 
 const signup_form = document.querySelector("#signUp_form");
-
 const logIn_form = document.querySelector("#logIn_form");
 
 signup_form.addEventListener("submit", (e) => {
@@ -52,9 +67,9 @@ logIn_form.addEventListener("submit",(e) => {
 
 });
 
-displayListing(2);
+displayListing(5);
 
-const logOut = document.querySelector(".log-out");
+
 logOut.addEventListener("click", () => {
     signOut();
 })
@@ -66,21 +81,20 @@ btn_searchHeader.addEventListener("click", () => {
 btn_searchMobile.addEventListener("click", () => {
     const searchWord = input_searchMobile.value;
     console.log(searchWord);
-    window.location.href =`collection.html?search=${searchWord}`;
+    if(searchWord) {
+        window.location.href =`collection.html?search=${searchWord}`;
+    }
+    
 });
 btn_searchHeader.addEventListener("click", () => {
     const searchWord = input_searchHeader.value;
     console.log(searchWord);
-    window.location.href =`collection.html?search=${searchWord}`;
+    if(searchWord) {
+        window.location.href =`collection.html?search=${searchWord}`;
+    }
+    
 });
 
-const create_form = document.querySelector("#create_form");
-
-const input_title = document.querySelector("#create_title");
-const input_description = document.querySelector("#create_description");
-const input_tags = document.querySelector("#create_tags");
-const input_media = document.querySelector("#create_media");
-const input_ends = document.querySelector("#create_endsAt");
 
 create_form.addEventListener("submit",(e) => {
     console.log("test");
